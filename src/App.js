3b4aa85489371bb66486148179f72d48a4e2a7c8
hanen96web <92207901+hanen96web@users.noreply.github.com>
2021-11-13 14:33:33 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>This is my profile</h1>
+      <Profile />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Profile() {
+  return (
+    <div className="ppp">
+
+    <img src="hanentt.png"></img>
+   <div className="hanendesign">
+   <h1> Name :Hanen Frioui</h1>
+   <h1>Date of birth:01/01/1996</h1>
+   <h1>Phone Number:23333595</h1>
+   <h1>Email:hanen.alfrioui96@gmail.com</h1>
+   <h1>Place of birth :Bechri souk lahad kebelli</h1>
+   </div>
+ 
+    </div>
+  );
+}
